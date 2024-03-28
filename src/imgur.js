f91a71717e07d42cdd73c10d6e5d1845f5d03c04
Jian Ting <me@jtpox.com>
2024-03-29 02:50:56 +0800

    Removed use of imgur package.

--- src/imgur.js
+++ src/imgur.js
@@ -2,12 +2,10 @@ const { writeFileSync, readFileSync } = require('fs');
 const { join } = require('path');
 const { Readable } = require('stream');
 const { Blob } = require('buffer');
-const { ImgurClient } = require('imgur');
 
 const { Info, Error } = require('./console');
 const { DEFAULT_IMAGE } = require('./common');
 
-let Client;
 let Roon;
 let ImgurClientId;
 
@@ -16,7 +14,8 @@ let Album = {
     deletehash: null,
 };
 
-const API_URL = 'https://api.imgur.com/3/image';
+const API_URL = 'https://api.imgur.com/3';
+const HEADERS = {};
 
 /**
  * Constructor for Imgur integration.
@@ -26,11 +25,8 @@ const API_URL = 'https://api.imgur.com/3/image';
  */
 function Initiate(roon, settings) {
     Roon = roon;
-    Client = new ImgurClient({
-        clientId: settings.imgurClientId,
-        clientSecret: settings.imgurClientSecret,
-    });
     ImgurClientId = settings.imgurClientId;
+    HEADERS.Authorization = `Client-ID ${ImgurClientId}`
 
     Album.id = settings.imgurAlbumId;
     Album.deletehash = settings.imgurAlbumDeleteHash;
@@ -53,15 +49,13 @@ async function UploadToAlbum(buffer, image_key) {
     data.append('description', image_key);
 
     try {
-        const upload = await fetch(API_URL, {
+        const upload = await fetch(`${API_URL}/image`, {
             method: 'POST',
-            headers: {
-                'Authorization': `Client-ID ${ImgurClientId}`,
-            },
+            headers: HEADERS,
             body: data,
         });
 
-        if(upload.status !== 200) {
+        if(!upload.ok) {
             Error(`Error uploading cover to Imgur with status: ${upload}`);
             return DEFAULT_IMAGE; 
         }
@@ -72,12 +66,16 @@ async function UploadToAlbum(buffer, image_key) {
         const albumForm = new FormData();
         albumForm.append('deletehashes[]', coverDeleteHash);
 
-        await fetch(API_URL, {
+        const albumUpdate = await fetch(`${API_URL}/album/${Album.deletehash}/add`, {
             method: 'POST',
-            headers: {
-                'Authorization': `Client-ID ${ImgurClientId}`,
-            },
+            headers: HEADERS,
+            body: albumForm,
         });
+
+        if(!albumUpdate.ok) {
+            Error(`Error adding cover to album.`);
+        }
+        const albumResponse = await albumUpdate.json();
 
         return link;
     } catch (err) {
@@ -91,17 +89,13 @@ async function UploadToAlbum(buffer, image_key) {
  * @async
  * @param {string} image_key Key of the image in the Imgur album.
  * @param {function(image_key:string) => Promise<Buffer|string>} GetImageFn 
- * @returns {Promise<string>} URL of album art or default iamge tag from Discord assets.
+ * @returns {Promise<string>} URL of album art or default image tag from Discord assets.
  */
 async function GetAlbumArt(image_key, GetImageFn) {
     return new Promise(async (resolve, reject) => {
         let album;
         try {
-            album = await Client.getAlbum(Album.id);
-            if(!album.success) {
-                Album = await CreateAlbum();
-                album = await Client.getAlbum(Album.id);
-            }
+            album = await GetAlbum();
         } catch (err) {}
 
         try {
@@ -138,16 +132,13 @@ async function GetAlbumArt(image_key, GetImageFn) {
 async function CreateAlbum() {
     const formData = new FormData();
     formData.append('title', 'Album Covers for Roon Discord Integration');
-    const albumFetch = await fetch(API_URL, {
+    const albumFetch = await fetch(`${API_URL}/album`, {
         method: 'POST',
-        headers: {
-            Authorization: `Client-ID ${ImgurClientId}`,
-        },
+        headers: HEADERS,
         body: formData,
     });
 
-    const albumDetails = await albumFetch.json();
-    const { data } = albumDetails;
+    const { data } = await albumFetch.json();
 
     const settings = Roon.load_config('settings') || Settings.DefaultSettings;
     settings.imgurAlbumId = data.id;
@@ -156,8 +147,37 @@ async function CreateAlbum() {
     Album.id = data.id;
     Album.deletehash = data.deletehash;
     Roon.save_config('settings', settings);
+    Info(`Imgur: Using Album ${Album.id}`);
 
     return data;
+}
+
+/**
+ * Get imags from anonymous Imgur album.
+ * @function GetAlbum
+ * @async
+ */
+async function GetAlbum() {
+    let output = {
+        success: false,
+        data: {},
+    };
+
+    const albumFetch = await fetch(`${API_URL}/album/${Album.id}/images`, {
+        method: 'GET',
+        headers: HEADERS,
+    });
+    if(!albumFetch.ok) {
+        Album = await CreateAlbum();
+        return GetAlbum();
+    }
+
+    const albumImages = await albumFetch.json();
+    output.success = true;
+    output.data = {
+        images: albumImages.data,
+    };
+    return output;
 }
 
 /** @module imgur */
