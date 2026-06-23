986c1001f06fd9a6f933575d5099283e1d9f3c1e
Jimmyscene <6708446+Jimmyscene@users.noreply.github.com>
2026-06-22 17:32:55 -0700

    bump

--- src/index.js
+++ src/index.js
@@ -252,7 +252,6 @@ async function SongChanged(core, data) {
   const endTimestamp = startTimestamp + length * 1000;
 
   const activity = {
-    name: `Listening to ${track} - ${artist}`,
     type: ActivityType.Listening,
     details: formatSongLine(track), // Track title
     state: formatSongLine(artist), // Track artist
